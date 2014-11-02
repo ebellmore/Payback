90785109965d805e6fe48a7f0bbbea4d454b60f6
Evan Backup <ebellmor@umass.edu>
2014-11-02 14:34:30 -0500

    Initial Commit

---
+++ Payback/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Payback
+//
+//  Created by Evan Backup on 11/2/14.
+//  Copyright (c) 2014 Evan Bellmore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Payback/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Payback
+//
+//  Created by Evan Backup on 11/2/14.
+//  Copyright (c) 2014 Evan Bellmore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
